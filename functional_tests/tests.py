5e53bde0fd914c66b9031a5af67bf9f0656c28e9
Cale Smith <calepayson@mac.com>
2023-08-04 08:59:37 -0700

    New URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,7 +4,7 @@ from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
 
-MAX_WAIT = 10
+MAX_WAIT = 5
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -33,7 +33,7 @@ class NewVisitorTest(LiveServerTestCase):
                 time.sleep(0.5)
 
 
-    def test_can_start_a_list_and_retrieve_it_later(self) -> None:
+    def test_can_start_a_list_for_one_user(self) -> None:
         # Edith has heard about a cool new online to-do app. She goes to check 
         # out its homepage
         self.browser.get(self.live_server_url)
@@ -71,6 +71,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfied, she goes back to sleep
 
+
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list
         self.browser.get(self.live_server_url)
@@ -85,9 +86,13 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Now a new user, Francis, comes along to the sitee
 
-        ## We use a neew browser session to make sure that no information of
+        ## We use a new browser session to make sure that no information of
         ## Edith's is coming through from cookies etc
         self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis visits the home page. There is no sign of Edith's list
+        self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
