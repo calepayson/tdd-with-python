554bacbeb948e921d4496168effa46dcf6388f6c
Cale Smith <calepayson@mac.com>
2023-08-20 10:56:36 -0700

    hack ALLOWED_HOSTS to be *

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x%^^dxhhhumtng3*bkp-fff&#*#%d9o++yc8zgmpte!oholdur'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
