938985710da7ceca491e0e2b793343d0a43d4fdf
Cale Smith <calepayson@mac.com>
2023-08-20 12:33:16 -0700

    set DEBUG back to "True"

--- superlists/settings.py
+++ superlists/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x%^^dxhhhumtng3*bkp-fff&#*#%d9o++yc8zgmpte!oholdur'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
